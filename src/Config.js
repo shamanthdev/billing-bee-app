aa6e44287c7c7f9150bd28eb83599af7e7db6502
shamantha.hegde <shamantha.hegde@6dtech.co.in>
2026-04-02 11:27:22 +0530

    base url added

---
+++ src/Config.js
@@ -0,0 +1,1 @@
+export const BASE_URL = "https://billing-bee-api.onrender.com/api";
--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+import { BASE_URL } from "../Config";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: BASE_URL,
 });
 
 export const setupAxiosInterceptors = (showLoader, hideLoader) => {
