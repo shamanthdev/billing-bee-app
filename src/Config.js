20a9afaaa7cf27cef15aee532883e1836329c9fb
shamantha.hegde <shamantha.hegde@6dtech.co.in>
2026-04-02 12:02:18 +0530

    connected frontend to live API

--- src/Config.js
+++ src/Config.js
@@ -1,1 +1,2 @@
-export const BASE_URL = "https://billing-bee-api.onrender.com/api";
+export const BASE_URL = process.env.REACT_APP_API_URL;
+
--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { BASE_URL } from "../Config";
 
 const api = axios.create({
-  baseURL: BASE_URL,
+  baseURL: `${BASE_URL}/api`,
 });
 
 export const setupAxiosInterceptors = (showLoader, hideLoader) => {
